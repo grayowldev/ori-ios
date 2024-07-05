091bf0ae7dbcb3836d72cb997deb0e45417d218d
kwasinsiah <truegrayowl@gmail.com>
2024-07-05 11:35:03 -0400

    Initial Commit

---
+++ Ori/OriApp.swift
@@ -0,0 +1,17 @@
+//
+//  OriApp.swift
+//  Ori
+//
+//  Created by Kwasi Nsiah on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
