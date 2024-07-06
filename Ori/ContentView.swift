c1054b09a46d6d5a3821865964455f79d2901163
kwasinsiah <truegrayowl@gmail.com>
2024-07-05 22:15:21 -0400

    Hello Ori

--- Ori/ContentView.swift
+++ Ori/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showsheet = false
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button {
+                showsheet = true
+            } label: {
+                Text("Click here")
+            }
         }
         .padding()
+        .sheet(isPresented: $showsheet) {
+            NavigationView {
+                        VStack {
+                            Text("This is a sheet view")
+                                .padding()
+                            Button("Dismiss") {
+                                dismiss()
+                            }
+                        }
+                        .navigationTitle("Sheet")
+                        .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
-#Preview {
-    ContentView()
+//#Preview {
+//    ContentView()
 }
