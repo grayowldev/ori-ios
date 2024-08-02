230d4e5cb2d51202150142ba92cef1400698d5dc
kwasinsiah <truegrayowl@gmail.com>
2024-08-02 03:04:41 -0400

    Updated Network Manager to fetch inventory items

--- Ori/NetworkManager.swift
+++ Ori/NetworkManager.swift
@@ -14,6 +14,7 @@ final class NetworkManager {
     static let baseURL = "https://tontally-core-production.up.railway.app/"
     
     private let orderingURL = baseURL + "ori/ordering"
+    private let inventoryURL = baseURL + "bulkItem/getAllBulkItems"
     
     private init() {}
     
@@ -45,6 +46,44 @@ final class NetworkManager {
             do {
                 let decoder = JSONDecoder()
                 let decodedResponse = try decoder.decode([OriOrder].self, from: data)
+                completed(.success(decodedResponse))
+            } catch {
+                completed(.failure(.invalidData))
+                print("in do failure")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getIInventoryItems(completed: @escaping (Result<[OriInventoryItem], APIError>) -> Void) {
+        guard let url = URL(string: inventoryURL) else {
+            completed(.failure(.invalidURL))
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if let _ =  error {
+                completed(.failure(.unableToComplete))
+                print("Unable to complete for some reason")
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200  else {
+                completed(.failure(.invalidResponse))
+                print("Invalid response, response code: ", response)
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                print("Invalid data")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode([OriInventoryItem].self, from: data)
                 completed(.success(decodedResponse))
             } catch {
                 completed(.failure(.invalidData))
