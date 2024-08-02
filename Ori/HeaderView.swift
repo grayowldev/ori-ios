3466683badf6da45d4f5af621f8b00688a041732
kwasinsiah <truegrayowl@gmail.com>
2024-08-02 03:06:57 -0400

    Updated table header for inventory items

--- Ori/HeaderView.swift
+++ Ori/HeaderView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct HeaderView: View {
+    var activeView: AvailableViews
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch activeView {
+        case .ordering:
+            HStack {
+                OrderIdCol(isHeader: true, text: "Order #")
+                Spacer()
+                TableCol(isHeader: true, text: "Description", width: 200)
+                Spacer()
+                TableCol(isHeader: true, text: "Gross Weight", width: 148)
+                Spacer()
+                TableCol(isHeader: true, text: "Stone Weight", width: 160)
+                Spacer()
+                TableCol(isHeader: true, text: "Net Weight", width: 160)
+                Spacer()
+                TableCol(isHeader: true, text: "Karat", width: 100)
+                Spacer()
+                TableCol(isHeader: true, text: "", width: 160)
+            }
+            .frame(height: 50)
+            .background(Color.black)
+            .foregroundColor(.white)
+        case .inventory:
+            HStack {
+                OrderIdCol(isHeader: true, text: "Item name")
+                Spacer()
+                TableCol(isHeader: true, text: "SKU", width: 200)
+                Spacer()
+                TableCol(isHeader: true, text: "Gross Weight", width: 148)
+                Spacer()
+                TableCol(isHeader: true, text: "Location", width: 160)
+                Spacer()
+                TableCol(isHeader: true, text: "", width: 160)
+            }
+            .frame(height: 50)
+            .background(Color.black)
+            .foregroundColor(.white)
+        }
+        
     }
 }
 
-#Preview {
-    HeaderView()
-}
+//#Preview {
+//    HeaderView()
+//}
